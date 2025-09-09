c3ccec46e7d950c6caf684c9e4f4a7877e6781c2
Avneesh Verma <162019333+Avya0503@users.noreply.github.com>
2025-09-10 00:13:41 +0530

    Time: 0 ms (100%), Space: 10.8 MB (69.97%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,12 +4,12 @@ public:
         int l=0;
         int mf=0;
         int res=0;
-        unordered_map<char,int> st;
+        vector<int> st(26,0);
         for(int r=0;r<s.size();r++){
-            st[s[r]]++;
-            mf=max(mf,st[s[r]]);
+            st[s[r]-'A']++;
+            mf=max(mf,st[s[r]-'A']);
             while(r-l+1-mf>k){
-                st[s[l]]--;
+                st[s[l]-'A']--;
                 l++;
             }
             res=max(res,r-l+1);
