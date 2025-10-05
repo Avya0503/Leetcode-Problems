e4b6161052debc94ca6152db273fe2dbc2c1ff6e
Avneesh Verma <162019333+Avya0503@users.noreply.github.com>
2025-10-05 20:12:21 +0530

    Time: 0 ms (100%), Space: 63.7 MB (12.05%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -5,10 +5,10 @@ public:
         int e=arr.size()-1;
         int mid=s+(e-s)/2;
         while(s<e){
-            if(arr[mid]<arr[mid+1])
-            s=mid+1;
-            else
+            if(arr[mid]>arr[mid+1])
             e=mid;
+            else
+            s=mid+1;
             mid=s+(e-s)/2;
         }
         return mid; 
