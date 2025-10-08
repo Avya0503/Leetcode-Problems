9bc771884a38c649316b5845ef37490c747b1952
Avneesh Verma <162019333+Avya0503@users.noreply.github.com>
2025-10-08 17:11:02 +0530

    Time: 0 ms (100%), Space: 10.6 MB (9.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-bool condition(char ch){
-    if(ch>='A'&& ch<='Z'||ch>='a'&& ch<='z'||ch>='0'&& ch<='9')
-    return true;
-    else 
-    return false;
-}
-char lower(char ch){
-    if(ch>='A'&& ch<='Z'){
-        ch=ch-'A'+'a';
-        return ch;
-    }
-    else
-    return ch;
-}
     bool isPalindrome(string s) {
+        vector<char> st;
+        for(char ch:s){
+            if(ch>='a'&&ch<='z'||ch>='0' && ch<='9')
+            st.push_back(ch);
+            else if(ch>='A'&&ch<='Z'){
+                ch=ch-'A'+'a';
+                st.push_back(ch);
+            }
+        }
         int i=0;
-        int e=s.size()-1;
+        int e=st.size()-1;
         while(i<e){
-            if(condition(s[i]) && condition(s[e])){
-                if(lower(s[i])!=lower(s[e]))
-                return false;
-            e--;
+            if(st[i]!=st[e])
+            return false;
             i++;
-            }
-            else{
-                if(condition(s[i]))
-                e--;
-                else
-                i++;
-            }
-           
+            e--;
         }
         return true;
+
     }
 };
